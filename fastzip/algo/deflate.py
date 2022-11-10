a2e63371b83cb9b31b0067dfba926a7912e30880
Tim Hatch <tim@timhatch.com>
2022-11-10 00:28:27 -0800

    Add tracing, fix accidentally-serial deflate

--- fastzip/algo/deflate.py
+++ fastzip/algo/deflate.py
@@ -2,6 +2,8 @@ import zlib
 from concurrent.futures import Executor, Future
 from typing import Iterable, Optional, Tuple, Union
 
+from keke import kev
+
 from ._base import BaseCompressor, parse_params
 from ._wrapfile import WrappedFile
 
@@ -39,32 +41,41 @@ class DeflateCompressor(BaseCompressor):
         # DEFLATE streams can be concatenated, as long as a Z_FINISH block is
         # not issued too early.
         _, m = file_object.mmapwrapper()
-        for start in block_starts:
-            yield pool.submit(
-                self._compress_block,
-                # TODO test with .mmapwrapper()
-                m[start : min(size, start + THREAD_BLOCK_SIZE)],
-                start == block_starts[-1],
-            )
+        with kev("pool.submit", __name__):
+            return [
+                pool.submit(
+                    self._compress_block,
+                    # TODO test with .mmapwrapper()
+                    m[start : min(size, start + THREAD_BLOCK_SIZE)],
+                    start == block_starts[-1],
+                )
+                for start in block_starts
+            ]
 
     def _compress_block(
         self, data: Union[bytes, memoryview], final: bool
     ) -> Tuple[bytes, int, int]:
-        # zlib compressobj are incredibly cheap, we'll just create a new one
-        # each time and let it go out of scope.
-        obj = zlib.compressobj(self._compresslevel, zlib.DEFLATED, -15)
-        # TODO benchmark to ensure the + isn't too expensive
-        buf: bytes = obj.compress(data)
+        with kev("compressobj", __name__):
+            # zlib compressobj are incredibly cheap, we'll just create a new one
+            # each time and let it go out of scope.
+            obj = zlib.compressobj(self._compresslevel, zlib.DEFLATED, -15)
+        with kev("compress", __name__, size=len(data)):
+            # TODO benchmark to ensure the + isn't too expensive
+            buf: bytes = obj.compress(data)
 
-        # ref https://www.bolet.org/~pornin/deflate-flush-fr.html
-        if final:
-            # Passing the correct arg here saves 6 bytes vs compressing the
-            # block with a full flush and appending a final flush!
-            buf += obj.flush(zlib.Z_FINISH)
-        else:
-            buf += obj.flush(zlib.Z_FULL_FLUSH)
+        with kev("flush", __name__, final=final):
+            # ref https://www.bolet.org/~pornin/deflate-flush-fr.html
+            if final:
+                # Passing the correct arg here saves 6 bytes vs compressing the
+                # block with a full flush and appending a final flush!
+                buf += obj.flush(zlib.Z_FINISH)
+            else:
+                buf += obj.flush(zlib.Z_FULL_FLUSH)
 
-        return (buf, len(data), zlib.crc32(data))
+        with kev("crc32", __name__):
+            crc = zlib.crc32(data)
+
+        return (buf, len(data), crc)
 
     def _decompress_for_testing(self, data: bytes) -> bytes:
         obj = zlib.decompressobj(-15)
