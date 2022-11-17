db65d18bccf1655f6ec5a27653413d0b49896323
Tim Hatch <tim@timhatch.com>
2022-11-17 07:48:35 -0800

    Fix typing for 3.8

--- fastzip/algo/_base.py
+++ fastzip/algo/_base.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from concurrent.futures import Executor, Future
 
 from typing import Dict, Optional, Sequence, Tuple, Union
--- fastzip/algo/store.py
+++ fastzip/algo/store.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from concurrent.futures import Executor, Future
 from typing import Optional, Sequence, Tuple, Union
 from zlib import crc32
